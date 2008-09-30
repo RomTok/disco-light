3d0939be133620100587bf4a7856e04e06cbff04
Matthias Hardt <MHardt@berlinux-solutions.de>
2008-09-30 17:54:45 +0200

    -added mysql database backend

---
+++ inc/mmstools/mmsdbmysql.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2008 by                                            *
+ *                                                                         *
+ *      Stefan Schwarzer <sxs@morphine.tv>                                 *
+ *      Guido Madaus     <bere@morphine.tv>                                *
+ *      Jens Schneider   <pupeider@morphine.tv>                            *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**
+ * @file  mmsdbmysql.h
+ *
+ * @brief Header file for mysql database functions.
+ *
+ * @author Stefan Schwarzer <sschwarzer@berlinux-solutions.de>
+ * @author Guido Madaus     <gmadaus@berlinux-solutions.de>
+ * @author Jens Schneider   <pupeider@morphine.tv>
+ * @author Matthias Hardt   <MHardt@berlinux-solutions.de>
+ *
+ * @ingroup mmstools
+ */
+#ifndef MMSDBMYSQL_H_
+#define MMSDBMYSQL_H_
+
+#ifdef __ENABLE_MYSQL__
+
+#include "mmstools/base.h"
+#include "mmstools/mmserror.h"
+#include "mmstools/mmsrecordset.h"
+#include "mmstools/datasource.h"
+#include "mmstools/interfaces/immsdb.h"
+
+#include <mysql.h>
+
+class MMSDBMySQL : public IMMSDB {
+    private:
+        MYSQL      dbhandle;
+
+    public:
+    	MMSDBMySQL(DataSource *datasource = NULL);
+        virtual ~MMSDBMySQL();
+
+        void connect();
+        void disconnect();
+        void startTransaction();
+        void commitTransaction();
+        void rollbackTransaction();
+        int query(string statement, MMSRecordSet *rs);
+        int query(string statement);
+        int getLastInsertedID();
+};
+
+#endif /*__ENABLE_MYSQL__*/
+
+#endif /*MMSDBMYSQL_H_*/
