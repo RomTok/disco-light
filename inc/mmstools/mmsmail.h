afb1de73d7858db7edc81f975442ebe659143692
Patrick Helterhoff <phelterhoff@berlinux-solutions.de>
2008-11-05 14:15:08 +0100

    - implemented MMSMail class for mailtransfer via a SMTP server (vmime required)

---
+++ inc/mmstools/mmsmail.h
@@ -0,0 +1,149 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2008 by                                            *
+ *                                                                         *
+ *      Stefan Schwarzer <sxs@morphine.tv>                                 *
+ *      Guido Madaus     <bere@morphine.tv>                                *
+ *      Jens Schneider   <pupeider@morphine.tv>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef MMSMAIL_H_
+#define MMSMAIL_H_
+
+#include "mmstools/base.h"
+#include "mmstools/mmserror.h"
+#include <vmime/vmime.hpp>
+#include <vmime/platforms/posix/posixHandler.hpp>
+#include <vector>
+
+/**
+ *  A smtp mail transfer class.
+ *
+ *  This class provides functions to to send e-mails via a smtp server.
+ *  Secure connections are note supported.
+ *
+ *  @author Patrick Helterhoff
+ */
+class MMSMail {
+private:
+	std::string							subject;
+	std::string							returnAddress;
+	std::vector<std::string>			recipients;
+	std::string							mailBody;
+	std::string						  	host;
+	vmime::ref<vmime::net::transport> 	transportService;
+
+public:
+	/**
+	 * Constructor of class MMSMail.
+	 *
+	 * @param _subject		 [in] the mail subject line
+	 * @param _returnAddress [in] the return address
+	 * @param _host			 [in] the smtp server name
+	 */
+	MMSMail(const std::string _subject, const std::string _returnAddress, const std::string _host);
+
+	/** Destructor of class MMSMail. */
+	~MMSMail();
+
+    /**
+     * Returns the mail subject line.
+     */
+	std::string getSubject() const;
+
+	/**
+	 * Sets the mail subject line.
+	 *
+	 * @param subject [in] the mail subject
+	 */
+    void setSubject(const std::string subject);
+
+    /**
+     * Returns the mails return addresss.
+     */
+    std::string getReturnAddress() const;
+    /**
+     * Sets the mails return address.
+     *
+     * @param returnAddress [in] the mails return address
+     */
+    void setReturnAddress(std::string returnAddress);
+
+    /**
+     * Returns the recipient at the given index.
+     *
+     * @param index [in] index of the recipient (first element = 0)
+     */
+    std::string getRecipient(int index) const;
+
+    /**
+     * Returns the current number of recipients.
+     */
+    const int getRecipientCount() const;
+
+    /**
+     * Adds a recipient to the current mail.
+     */
+    void addRecipient(std::string recipient);
+
+    /**
+     * Removes a recipient at the given index.
+     *
+     * @param index [in] index of the recipient (first element = 0)
+     */
+    void removeRecipient(int index);
+
+    /**
+     * Returns the mails message text.
+     */
+    std::string getMailBody() const;
+
+    /**
+     * Sets the mails message text.
+     *
+     * @param mailBody [in] Message text of the mail
+     */
+    void setMailBody(std::string mailBody);
+
+    /**
+     * Returns the current mail servers address.
+     */
+    std::string getHost() const;
+
+    /**
+     * Sets the mail servers address.
+     *
+     * @param host [in] the mail servers address
+     */
+    void setHost(std::string host);
+
+    /**
+     * Use this to set user and password for the smtp server connection, if necessary.
+     *
+     * @param   user    	[in] the ftp user
+     * @param   password   	[in] the password
+     */
+    void setAuthData(const std::string &user, const std::string &password);
+
+    /**
+     * Sends the mail to the specified recipients.
+     *
+     * @throws MMSError if errors occure during the mail send.
+     */
+    void send();
+};
+
+#endif /* MMSMAIL_H_ */
---
+++ src/mmstools/mmsmail.cpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2008 by                                            *
+ *                                                                         *
+ *      Stefan Schwarzer <sxs@morphine.tv>                                 *
+ *      Guido Madaus     <bere@morphine.tv>                                *
+ *      Jens Schneider   <pupeider@morphine.tv>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "mmstools/mmsmail.h"
+
+MMSMail::MMSMail(const std::string _subject, const std::string _returnAddress, const std::string _host) :
+	subject(_subject), returnAddress(_returnAddress) {
+	vmime::platform::setHandler<vmime::platforms::posix::posixHandler>();
+	setHost(_host);
+}
+
+MMSMail::~MMSMail() {
+
+}
+
+std::string MMSMail::getSubject() const {
+    return this->subject;
+}
+
+void MMSMail::setSubject(const std::string subject) {
+    this->subject = subject;
+}
+
+std::string MMSMail::getReturnAddress() const {
+    return this->returnAddress;
+}
+
+void MMSMail::setReturnAddress(std::string returnAddress) {
+    this->returnAddress = returnAddress;
+}
+
+std::string MMSMail::getRecipient(int index) const {
+	if (!this->recipients.empty() && index < this->recipients.size())
+		return this->recipients.at(index);
+	else
+		return NULL;
+}
+
+const int MMSMail::getRecipientCount() const {
+	return this->recipients.size();
+}
+
+void MMSMail::addRecipient(std::string recipient) {
+	this->recipients.push_back(recipient);
+}
+
+void MMSMail::removeRecipient(int index) {
+	this->recipients.erase(this->recipients.begin() + index);
+}
+
+std::string MMSMail::getMailBody() const {
+    return mailBody;
+}
+
+void MMSMail::setMailBody(std::string mailBody) {
+    this->mailBody = mailBody;
+}
+
+std::string MMSMail::getHost() const {
+	return this->host;
+}
+
+void MMSMail::setHost(std::string host) {
+	this->host = host;
+
+	std::string hoststring = "smtp://" + host;
+	vmime::utility::url url(hoststring);
+	vmime::ref <vmime::net::session> sess = vmime::create <vmime::net::session>();
+	this->transportService = sess->getTransport(url);
+}
+
+void MMSMail::setAuthData(const std::string &user, const std::string &password) {
+	this->transportService->setProperty("options.need-authentication", true);
+	this->transportService->setProperty("auth.username", user);
+	this->transportService->setProperty("auth.password", password);
+}
+
+void MMSMail::send() {
+	try {
+		vmime::messageBuilder mb;
+		mb.setSubject(vmime::text(this->subject));
+		mb.setExpeditor(vmime::mailbox(this->returnAddress));
+
+		for (std::vector<std::string>::iterator i = this->recipients.begin(); i != this->recipients.end(); i++) {
+			mb.getRecipients().appendAddress(vmime::create<vmime::mailbox>(*i));
+		}
+
+		mb.getTextPart()->setCharset(vmime::charsets::ISO8859_15);
+		mb.getTextPart()->setText(vmime::create<vmime::stringContentHandler>(this->mailBody));
+		vmime::ref<vmime::message> msg = mb.construct();
+
+		this->transportService->connect();
+		this->transportService->send(msg);
+		this->transportService->disconnect();
+	}
+	catch (vmime::exception& e)
+	{
+		throw new MMSError(0, e.what());
+	}
+	catch (std::exception& e)
+	{
+		throw new MMSError(0, e.what());
+	}
+}
