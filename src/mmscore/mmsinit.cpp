dde82aaefa234391c391283b4c8b079311d57360
Matthias Hardt <MHardt@berlinux-solutions.de>
2008-12-05 16:46:45 +0100

    -added missing system header

--- src/mmscore/mmsinit.cpp
+++ src/mmscore/mmsinit.cpp
@@ -20,8 +20,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstring>
 #include "mmscore/mmsinit.h"
-
 #include "mms.h"
 
 static MMSPluginManager             *pluginmanager      = NULL;
