cbc997a5ba80771817dd54d5f8df6afaad1ded7f
Matthias Hardt <MHardt@berlinux-solutions.de>
2008-12-05 16:54:16 +0100

    -added missing system header

--- src/mmsgui/fb/mmsfbwindowmanager.cpp
+++ src/mmsgui/fb/mmsfbwindowmanager.cpp
@@ -20,6 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstring>
 #include "mmsgui/fb/mmsfbwindowmanager.h"
 #include "mmsinfo/mmsinfo.h"
 #include "mmsgui/fb/mmsfb.h"
