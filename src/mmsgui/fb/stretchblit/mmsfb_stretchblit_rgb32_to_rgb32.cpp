ba7d761a20e74964be00abf9fecc1515b4de8ab7
Jens Schneider <pupeider@gmx.de>
2009-01-27 18:25:27 +0100

    new stretchblit routines

---
+++ src/mmsgui/fb/stretchblit/mmsfb_stretchblit_rgb32_to_rgb32.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 Stefan Schwarzer, Jens Schneider,             *
+ *                           Matthias Hardt, Guido Madaus                  *
+ *                                                                         *
+ *   Copyright (C) 2007-2008 Berlinux Solutions GbR                        *
+ *                           Stefan Schwarzer & Guido Madaus               *
+ *                                                                         *
+ *   Authors:                                                              *
+ *      Stefan Schwarzer <SSchwarzer@berlinux-solutions.de>,               *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>,                   *
+ *      Jens Schneider   <pupeider@gmx.de>                                 *
+ *      Guido Madaus     <GMadaus@berlinux-solutions.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <cstring>
+#include "mmsgui/fb/mmsfbconv.h"
+#include "mmstools/mmstools.h"
+
+void mmsfb_stretchblit_rgb32_to_rgb32(MMSFBExternalSurfaceBuffer *extbuf, int src_height, int sx, int sy, int sw, int sh,
+									  unsigned int *dst, int dst_pitch, int dst_height, int dx, int dy, int dw, int dh,
+									  bool antialiasing) {
+	// first time?
+	static bool firsttime = true;
+	if (firsttime) {
+		printf("DISKO: Using accelerated stretch RGB32 to RGB32.\n");
+		firsttime = false;
+	}
+
+	// no difference to argb, use argb to argb routine
+	mmsfb_stretchblit_argb_to_argb(extbuf, src_height, sx, sy, sw, sh,
+								   dst, dst_pitch, dst_height, dx, dy, dw, dh, antialiasing);
+}
+
+
