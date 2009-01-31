8be1cd6d508b47e2781f44e195b954b8d7d408e9
Jens Schneider <pupeider@gmx.de>
2009-01-31 13:36:59 +0100

    four new pixel conversion routines

---
+++ src/mmsgui/fb/stretchblit/mmsfb_stretchblit_rgb24_to_argb.cpp
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 Stefan Schwarzer, Jens Schneider,             *
+ *                           Matthias Hardt, Guido Madaus                  *
+ *                                                                         *
+ *   Copyright (C) 2007-2008 Berlinux Solutions GbR                        *
+ *                           Stefan Schwarzer & Guido Madaus               *
+ *                                                                         *
+ *   Authors:                                                              *
+ *      Stefan Schwarzer <SSchwarzer@berlinux-solutions.de>,               *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>,                   *
+ *      Jens Schneider   <pupeider@gmx.de>                                 *
+ *      Guido Madaus     <GMadaus@berlinux-solutions.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "mmsgui/fb/mmsfbconv.h"
+#include "mmstools/mmstools.h"
+
+void mmsfb_stretchblit_argb_to_argb(MMSFBExternalSurfaceBuffer *extbuf, int src_height, int sx, int sy, int sw, int sh,
+									unsigned int *dst, int dst_pitch, int dst_height, int dx, int dy, int dw, int dh,
+									bool antialiasing) {
+	// first time?
+	static bool firsttime = true;
+	if (firsttime) {
+		printf("DISKO: Using accelerated stretch ARGB to ARGB.\n");
+		firsttime = false;
+	}
+
+	// get the first source ptr/pitch
+	unsigned int *src = (unsigned int *)extbuf->ptr;
+	int src_pitch = extbuf->pitch;
+
+	// prepare...
+	int src_pitch_pix = src_pitch >> 2;
+	int dst_pitch_pix = dst_pitch >> 2;
+
+	// check the surface range
+	if ((sw <= 0)||(sh <= 0))
+		return;
+
+	// antialiasing horizontal/vertical/both
+	bool h_antialiasing = false;
+	bool v_antialiasing = false;
+	if (antialiasing) {
+		h_antialiasing = true;
+		if (sh != dh)
+			v_antialiasing = true;
+	}
+
+	// stretch buffer
+	stretch_uint_buffer(h_antialiasing, v_antialiasing,
+						src, src_pitch, src_pitch_pix, src_height, sx, sy, sw, sh,
+					    dst, dst_pitch, dst_pitch_pix, dst_height, dx, dy, dw, dh);
+}
+
