ed005a752d41da4c196e6ee983277a66f37e2c06
Jens Schneider <pupeider@gmx.de>
2009-02-02 15:19:48 +0100

    new mmswidget thread

---
+++ src/mmsgui/mmswidgetthread.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 Stefan Schwarzer, Jens Schneider,             *
+ *                           Matthias Hardt, Guido Madaus                  *
+ *                                                                         *
+ *   Copyright (C) 2007-2008 Berlinux Solutions GbR                        *
+ *                           Stefan Schwarzer & Guido Madaus               *
+ *                                                                         *
+ *   Authors:                                                              *
+ *      Stefan Schwarzer <SSchwarzer@berlinux-solutions.de>,               *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>,                   *
+ *      Jens Schneider   <pupeider@gmx.de>                                 *
+ *      Guido Madaus     <GMadaus@berlinux-solutions.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "mmsgui/mmswidgetthread.h"
+
+MMSWidgetThread::MMSWidgetThread(MMSWidget *widget) {
+	this->widget = widget;
+	this->delay = 0;
+}
+
+void MMSWidgetThread::threadMain() {
+	if (!this->widget)
+		return;
+
+	if (this->delay <= 0)
+		return;
+
+	while (this->delay > 0) {
+		usleep(50000);
+		this->delay -= 50;
+	}
+
+	this->widget->setFocus(false);
+}
+
+void MMSWidgetThread::start(int delay) {
+	this->delay = delay * 1000;
+	MMSThread::start();
+}
+
