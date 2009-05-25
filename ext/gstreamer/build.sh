2b58ac7fc04a88fc2a0f480ac3ac30cb10defc45
Matthias Hardt <MHardt@berlinux-solutions.de>
2009-05-25 17:34:25 +0200

    ext: -gstreamer build script enhanced

--- ext/gstreamer/build.sh
+++ ext/gstreamer/build.sh
@@ -1,27 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+########################################################################
+# Script to install gstreamer including the disko videosink plugin.
+#
+# usage: ./build.sh [options]
+#
+#     -h)        print this help text
+#     -i prefix) set installation prefix for gstreamer
+########################################################################
 
-if [ ! -d gstreamer-0.10.23 ]; then
+function usage() {
+    echo "usage: $0 [option]"
+    echo
+    echo "    -h)        print this help text"
+    echo "    -i prefix) set installation prefix for gstreamer"
+}
+
+while getopts  "hi:" flag; do
+    case $flag in
+        h) usage; exit 1;;
+        i) installprefix="--prefix=$OPTARG"; 
+           export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$OPTARG/lib/pkgconfig;;
+    esac
+done
+
+[ ! -f gstreamer-0.10.23.tar.bz2 ] && \
     wget http://gstreamer.freedesktop.org/src/gstreamer/gstreamer-0.10.23.tar.bz2;
+    
+[ ! -d gstreamer-0.10.23 ] && \
     tar jxf gstreamer-0.10.23.tar.bz2;
-    cd gstreamer-0.10.23;
-    ./configure && make && make install;
-    cd -
-fi;
 
-if [ ! -d gst-plugins-base-0.10.23 ]; then
+cd gstreamer-0.10.23;
+./configure "$installprefix" && make && make install;
+cd -
+
+[ ! -f gst-plugins-base-0.10.23.tar.bz2 ] && \
     wget http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.23.tar.bz2;
+    
+[ ! -d gst-plugins-base-0.10.23 ] && \
     tar jxf gst-plugins-base-0.10.23.tar.bz2;
-    cd gst-plugins-base-0.10.23;
-    ./configure && make && make install;
-    cd -
-fi;
 
-if [ ! -d gst-plugins-bad-0.10.12 ]; then
+cd gst-plugins-base-0.10.23;
+./configure "$installprefix" && make && make install;
+cd -
+
+[ ! -f gst-plugins-bad-0.10.12 ] && \
     wget http://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-0.10.12.tar.bz2;
-    tar jxf gst-plugins-bad-0.10.12.tar.bz2;
-    cd gst-plugins-bad-0.10.12;
-    patch -p1 < gst-plugins-bad-0.10.12_diskovideosink.diff;
-    ./configure && make && make install;
-    cd -
-fi;
 
+[ ! -d gst-plugins-bad-0.10.12 ] && \
+    tar jxf gst-plugins-bad-0.10.12.tar.bz2;
+
+cd gst-plugins-bad-0.10.12;
+
+[ ! -d ext/disko ] && \
+    patch -p1 < gst-plugins-bad-0.10.12_diskovideosink.diff;
+    
+./configure "$installprefix" && make && make install;
