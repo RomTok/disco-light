480350cc2ab039b5a0eab443c4a9d57e3ec470ac
Jens Schneider <pupeider@gmx.de>
2009-06-20 23:45:37 +0200

    new yuy2 fillrectangle

---
+++ src/mmsgui/fb/fillrectangle/mmsfb_fillrectangle_yuy2.cpp
@@ -0,0 +1,95 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 Stefan Schwarzer, Jens Schneider,             *
+ *                           Matthias Hardt, Guido Madaus                  *
+ *                                                                         *
+ *   Copyright (C) 2007-2008 Berlinux Solutions GbR                        *
+ *                           Stefan Schwarzer & Guido Madaus               *
+ *                                                                         *
+ *   Authors:                                                              *
+ *      Stefan Schwarzer <SSchwarzer@berlinux-solutions.de>,               *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>,                   *
+ *      Jens Schneider   <pupeider@gmx.de>                                 *
+ *      Guido Madaus     <GMadaus@berlinux-solutions.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "mmsgui/fb/mmsfbconv.h"
+#include "mmstools/mmstools.h"
+
+void mmsfb_fillrectangle_yuy2(MMSFBSurfacePlanes *dst_planes, int dst_height,
+						      int dx, int dy, int dw, int dh, MMSFBColor color) {
+	// first time?
+	static bool firsttime = true;
+	if (firsttime) {
+		printf("DISKO: Using accelerated fill rectangle to YUY2.\n");
+		firsttime = false;
+	}
+
+	// get the first destination ptr/pitch
+	unsigned int *dst = (unsigned int *)dst_planes->ptr;
+	int dst_pitch = dst_planes->pitch;
+
+	// dest x and w must be multiple of two
+	// reason: YUY2 codes U/V values separated in two horizontal pixels
+	if (dx & 0x01) {
+		dx++;
+		dw--;
+	}
+	dw/=2;
+
+	// prepare...
+	int dst_pitch_pix = dst_pitch >> 2;
+	dst+= dx + dy * dst_pitch_pix;
+
+	unsigned int *dst_end = dst + dst_pitch_pix * dh;
+#ifndef __HAVE_SSE__
+	int dst_pitch_diff = dst_pitch_pix - dw;
+#endif
+
+	// prepare the color
+	unsigned int SRC_Y = MMSFB_CONV_RGB2Y(color.r, color.g, color.b);
+	unsigned int SRC_U = MMSFB_CONV_RGB2U(color.r, color.g, color.b);
+	unsigned int SRC_V = MMSFB_CONV_RGB2V(color.r, color.g, color.b);
+	register unsigned int SRC;
+	SRC =     (SRC_V << 24)
+			| (SRC_Y << 16)
+			| (SRC_U << 8)
+			|  SRC_Y;
+
+	// copy pixel directly to the destination
+	// for all lines
+	while (dst < dst_end) {
+		// for all pixels in the line
+#ifdef __HAVE_SSE__
+		// fill memory 4-byte-wise (much faster than loop see below)
+		__asm__ __volatile__ ( "\trep stosl\n" : : "D" (dst), "a" (SRC), "c" (dw));
+
+		// go to the next line
+		dst+= dst_pitch_pix;
+#else
+		unsigned int *line_end = dst + dw;
+		while (dst < line_end) {
+			*dst = SRC;
+			dst++;
+		}
+
+		// go to the next line
+		dst+= dst_pitch_diff;
+#endif
+	}
+}
+
+
