3c4c84ff0f9c7b26a30668f868996e97b8536d87
Jens Schneider <pupeider@gmx.de>
2009-06-20 23:12:43 +0200

    new yuy2 to yuy2 blit

---
+++ src/mmsgui/fb/blit/mmsfb_blit_yuy2_to_yuy2.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 Stefan Schwarzer, Jens Schneider,             *
+ *                           Matthias Hardt, Guido Madaus                  *
+ *                                                                         *
+ *   Copyright (C) 2007-2008 Berlinux Solutions GbR                        *
+ *                           Stefan Schwarzer & Guido Madaus               *
+ *                                                                         *
+ *   Authors:                                                              *
+ *      Stefan Schwarzer <SSchwarzer@berlinux-solutions.de>,               *
+ *      Matthias Hardt   <MHardt@berlinux-solutions.de>,                   *
+ *      Jens Schneider   <pupeider@gmx.de>                                 *
+ *      Guido Madaus     <GMadaus@berlinux-solutions.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <cstring>
+#include "mmsgui/fb/mmsfbconv.h"
+#include "mmstools/mmstools.h"
+
+void mmsfb_blit_yuy2_to_yuy2(MMSFBSurfacePlanes *src_planes, int src_height, int sx, int sy, int sw, int sh,
+							 MMSFBSurfacePlanes *dst_planes, int dst_height, int dx, int dy) {
+	// first time?
+	static bool firsttime = true;
+	if (firsttime) {
+		printf("DISKO: Using accelerated copy YUY2 to YUY2.\n");
+		firsttime = false;
+	}
+
+	// get the first source ptr/pitch
+	unsigned short int *src = (unsigned short int *)src_planes->ptr;
+	int src_pitch = src_planes->pitch;
+
+	// get the first destination ptr/pitch
+	unsigned short int *dst = (unsigned short int *)dst_planes->ptr;
+	int dst_pitch = dst_planes->pitch;
+
+	// source/dest x and w must be multiple of two
+	// reason: YUY2 codes U/V values separated in two horizontal pixels
+	if (sx & 0x01) {
+		sx++;
+		sw--;
+		dx++;
+	}
+	if (sw & 0x01) {
+		sw--;
+		if (dx & 0x01) {
+			dx++;
+		}
+	}
+	else
+	if (dx & 0x01) {
+		dx++;
+		sw-=2;
+	}
+
+	// prepare...
+	int src_pitch_pix = src_pitch >> 1;
+	int dst_pitch_pix = dst_pitch >> 1;
+	src+= sx + sy * src_pitch_pix;
+	dst+= dx + dy * dst_pitch_pix;
+
+	// check the surface range
+	if (dst_pitch_pix - dx < sw - sx)
+		sw = dst_pitch_pix - dx - sx;
+	if (dst_height - dy < sh - sy)
+		sh = dst_height - dy - sy;
+	if ((sw <= 0)||(sh <= 0))
+		return;
+
+	unsigned short int *src_end = src + src_pitch_pix * sh;
+
+	// for all lines
+	while (src < src_end) {
+		// copy the line
+		memcpy(dst, src, sw << 1);
+
+		// go to the next line
+		src+= src_pitch_pix;
+		dst+= dst_pitch_pix;
+	}
+}
+
+
