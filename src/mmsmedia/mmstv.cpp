82cdf7da8d5273454f6d157825243b7dbd78413b
Stefan Schwarzer <sschwarzer@berlinux-solutions.de>
2009-11-01 15:53:15 +0100

    mmstv: - added disko.h

--- src/mmsmedia/mmstv.cpp
+++ src/mmsmedia/mmstv.cpp
@@ -29,7 +29,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
  **************************************************************************/
-
+#include "disko.h"
 #include "mmsmedia/mmstv.h"
 #include <strings.h>
 
