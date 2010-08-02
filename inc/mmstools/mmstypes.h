e9f9f16db0331650007f04934d1c608d26eb2382
Jens Schneider <jens.schneider@diskohq.org>
2010-08-02 16:44:15 +0200

    bugfix: constructors of MMSFBRegion and MMSFBTriangle corrected

--- inc/mmstools/mmstypes.h
+++ inc/mmstools/mmstypes.h
@@ -547,7 +547,7 @@ class MMSFBRegion {
 			this->x1 = x1;
 			this->y1 = y1;
 			this->x2 = x2;
-			this->y2 = x2;
+			this->y2 = y2;
 		}
 };
 
@@ -571,9 +571,9 @@ class MMSFBTriangle {
 			this->x1 = x1;
 			this->y1 = y1;
 			this->x2 = x2;
-			this->y2 = x2;
+			this->y2 = y2;
 			this->x3 = x3;
-			this->y3 = x3;
+			this->y3 = y3;
 		}
 };
 
