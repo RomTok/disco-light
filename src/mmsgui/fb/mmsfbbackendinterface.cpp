7f2936400af0f2e7b6eceff7d9a40119f2a09a67
Jens Schneider <jens.schneider@diskohq.org>
2012-01-15 15:56:11 +0100

    MMSFBFont/OGL: cleanup

--- src/mmsgui/fb/mmsfbbackendinterface.cpp
+++ src/mmsgui/fb/mmsfbbackendinterface.cpp
@@ -1205,8 +1205,15 @@ void MMSFBBackEndInterface::processDrawString(BEI_DRAWSTRING *req) {
 		DY = req->surface->config.font->height;
 		DY -= req->surface->config.font->descender + 1;
 
+#ifdef __HAVE_GLU__
+		// calc base position
 		req->x = (int)((float)req->x / scale_coeff + 0.5f);
 		req->y = (int)((float)req->y / scale_coeff + 0.5f);
+
+		// calc subsurface offsets
+		xoff = (int)((float)xoff / scale_coeff + 0.5f);
+		yoff = (int)((float)yoff / scale_coeff + 0.5f);
+#endif
 
 		// for all characters
 		MMSFBFONT_GET_UNICODE_CHAR(req->text, req->len) {
@@ -1217,22 +1224,14 @@ void MMSFBBackEndInterface::processDrawString(BEI_DRAWSTRING *req) {
 			// start rendering of glyph to destination
 			if (glyph_loaded) {
 				// calc destination position of the character
-/*				int dx = req->x + glyph.left;
-				int dy = req->y + DY - glyph.top;*/
-//				int dx = req->x + glyph.left;
-//				int dx = req->x + glyph.left;
-//				int dx = (float)req->x / scale_coeff + glyph.left;
-//				int dx = req->x + glyph.left;
-//				int dy = (float)req->y / scale_coeff + DY - glyph.top;
-
 				int dx = req->x + glyph.left;
 				int dy = req->y + DY - glyph.top;
 
 				// get destination region
-				int dx1 = dx + (int)((float)xoff / scale_coeff + 0.5f);
-				int dy1 = dy + (int)((float)yoff / scale_coeff + 0.5f);
-				int dx2 = dx + src_w - 1 + (int)((float)xoff / scale_coeff + 0.5f);
-				int dy2 = dy + src_h - 1 + (int)((float)yoff / scale_coeff + 0.5f);
+				int dx1 = dx + xoff;
+				int dy1 = dy + yoff;
+				int dx2 = dx + src_w - 1 + xoff;
+				int dy2 = dy + src_h - 1 + yoff;
 
 #ifndef __HAVE_GLU__
 				// get source region
@@ -1247,12 +1246,6 @@ void MMSFBBackEndInterface::processDrawString(BEI_DRAWSTRING *req) {
 										dx1, dy1, dx2, dy2);
 #else
 				// move to correct position
-/*				dx1 = ((float)dx1 + 0.5f) / scale_coeff;
-				dy1 = ((float)dy1 + 0.5f) / scale_coeff;*/
-//mmsfbgl.pushCurrentMatrix();
-//				mmsfbgl.translateCurrentMatrix(dx1, dy1, 0);
-//mmsfbgl.scaleCurrentMatrix(scale_coeff, scale_coeff, 1);
-
 				mmsfbgl.translateCurrentMatrix(dx1 - dx1_save, dy1 - dy1_save, 0);
 				dx1_save = dx1;
 				dy1_save = dy1;
@@ -1289,7 +1282,6 @@ void MMSFBBackEndInterface::processDrawString(BEI_DRAWSTRING *req) {
 						mmsfbgl.drawElements(&vertex_buffer->arrays[m], NULL, NULL, &index_buffer->arrays[m]);
 					}
 				}
-//mmsfbgl.popCurrentMatrix();
 
 #endif
 
